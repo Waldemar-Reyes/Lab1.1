b52857d30c516bfaa9283df7d2cd9f411f6a50ff
Waldemar-Reyes <waldemar.reyes@upr.edu>
2018-03-04 22:49:29 -0400

    Homework Implementation

--- src/orderedStructures/Arithmetic.java
+++ src/orderedStructures/Arithmetic.java
@@ -1,6 +1,8 @@
 package orderedStructures;
 
-public class Arithmetic extends Progression {
+import interfaces.Combinable;
+
+public class Arithmetic extends Progression implements Combinable {
 	private double commonDifference; 
 	
 	public Arithmetic(double firstValue, double commonDifference) { 
@@ -27,6 +29,18 @@ public class Arithmetic extends Progression {
 	public double getTerm(int n) {
 		double value = this.firstValue() + commonDifference * (n-1);
 		return value;
+	}
+
+	@Override
+	public Progression add(Progression p) {
+		p = new Arithmetic( ((Arithmetic)p).firstValue() + this.firstValue(), ((Arithmetic)p).commonDifference + this.commonDifference );
+		return p;
+	}
+
+	@Override
+	public Progression substract(Progression p) {
+		p = new Arithmetic( ((Arithmetic)p).firstValue() - this.firstValue(), Math.abs(((Arithmetic)p).commonDifference - this.commonDifference));
+		return p;
 	}
 
 }
